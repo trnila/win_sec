0bf2a43a9044ef4756f56f0954df41edcab72b36
Daniel Trnka <daniel.trnka@gmail.com>
2019-04-05 20:32:58 +0200

    added c/common.mk for dlls

--- 5/c/common.mk
+++ 5/c/common.mk
@@ -1,9 +1,16 @@
 CC=x86_64-w64-mingw32-gcc
+CPP=x86_64-w64-mingw32-g++
 
 all: $(TARGETS)
 
 %.exe: %.c
 	$(CC) $(CFLAGS) $^ -o $@
+
+%.exe: %.cpp
+	$(CPP) $(CFLAGS) $^ -o $@
+
+%.dll: %.c
+	$(CC) $(CFLAGS) -shared $^ -o $@
 
 clean:
 	rm $(TARGETS)
